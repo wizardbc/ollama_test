eaf6cdecebd2115adf3bdcde14e118b778ce7d64
Byung Chun Kim <wizardbc@gmail.com>
2024-12-23 16:26:34 +0000

    ollama list and image on sidebar

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import requests
 from PIL import Image
 from langchain_ollama import ChatOllama
+import ollama
 
 st.set_page_config(layout="wide")
 
@@ -50,7 +51,8 @@ def _load_llm(model_name:str, temperature:float, top_k:int, top_p:float):
 
 with st.sidebar:
     st.header("Model")
-    model_name = st.selectbox("model", ['llama3.3', 'llama3.2-vision:90b'])
+    model_name = st.selectbox("model", [m.model for m in ollama.list().models])
+    _is_vision = (model_name.split(':')[0].split('-')[-1] == "vision")
     temperature = st.slider("temperature", min_value=0.0, max_value=2.0, value=0.8)
     top_k = st.number_input("top_k", min_value=1, value=40)
     top_p = st.slider("top_p", min_value=0.0, max_value=1.0, value=0.9)
@@ -71,22 +73,6 @@ for message in st.session_state.messages:
                 else:
                     st.markdown(content["text"])
 
-_is_vision = (model_name.split(':')[0].split('-')[-1] == "vision")
-if (role == 'human') and _is_vision:
-    image_base64 = None
-    url = st.text_input("Image URL", key=f"url_{len(st.session_state.messages)}")
-    if url:
-        response = requests.get(url)
-        _image = Image.open(BytesIO(response.content))
-        st.image(_image)
-        image_base64 = f"data:image/png;base64,{convert_to_base64(_image)}"
-    else:
-        image = st.file_uploader("Upload an image", type=["jpg","png","jpeg"], key=f"img_{len(st.session_state.messages)}")
-        if image:
-            with st.chat_message(role):
-                st.image(image)
-            image_base64 = f"data:image/png;base64,{convert_to_base64(Image.open(image))}"
-
 chat_area = st.container()
 def gen_res():
     with chat_area:
@@ -95,6 +81,30 @@ def gen_res():
             response = st.write_stream(llm.stream(st.session_state.messages))
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+with st.sidebar:
+    btn_col1, btn_col2 = st.columns(2)
+    with btn_col1:
+        st.button("Rewind", on_click=rewind, use_container_width=True, type='primary')
+    with btn_col2:
+        st.button("Clear", on_click=clear, use_container_width=True)
+    st.button("Generate", on_click=gen_res, use_container_width=True)
+
+    if (role == 'human') and _is_vision:
+        st.header("Vision")
+        image_base64 = None
+        url = st.text_input("Image URL", key=f"url_{len(st.session_state.messages)}")
+        if url:
+            response = requests.get(url)
+            _image = Image.open(BytesIO(response.content))
+            st.image(_image)
+            image_base64 = f"data:image/png;base64,{convert_to_base64(_image)}"
+        else:
+            image = st.file_uploader("Upload an image", type=["jpg","png","jpeg"], key=f"img_{len(st.session_state.messages)}")
+            if image:
+                with st.chat_message(role):
+                    st.image(image)
+                image_base64 = f"data:image/png;base64,{convert_to_base64(Image.open(image))}"
 
 if prompt := st.chat_input("Your message"):
     with chat_area:
@@ -117,12 +127,3 @@ if prompt := st.chat_input("Your message"):
     if role == "human":
         gen_res()
     st.rerun()
-
-
-with st.sidebar:
-    btn_col1, btn_col2 = st.columns(2)
-    with btn_col1:
-        st.button("Rewind", on_click=rewind, use_container_width=True, type='primary')
-    with btn_col2:
-        st.button("Clear", on_click=clear, use_container_width=True)
-    st.button("Generate", on_click=gen_res, use_container_width=True)
